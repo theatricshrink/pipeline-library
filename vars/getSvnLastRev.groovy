e8478494e2f63175471c6f29d428131ba46cac47
theatricshrink <christian.kerth@me.com>
2017-02-23 16:17:10 +0100

    Add files via upload

--- vars/getSvnLastRev.groovy
+++ vars/getSvnLastRev.groovy
@@ -1,11 +1,4 @@
-@NonCPS
-def call(String svnUrl) {
-	def svnInfo = "";
-	if(isUnix) {
-		def svnInfo = sh script: "svn info ${svnUrl}", returnStdout: true
-	} else {
-		def svnInfo = bat script: "svn info ${svnUrl}", returnStdout: true
-	}
+def call(String svnInfo) {
     def matcher = svnInfo =~ 'Letzte.*Rev:\\s(\\d+)'
 	return matcher ? matcher[0][1] : null
 }
