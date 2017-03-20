d606963e49e04d04f5274c5518ceca258386215b
theatricshrink <christian.kerth@me.com>
2017-03-20 15:57:16 +0100

    Update readGlobalConfig.groovy

--- vars/readGlobalConfig.groovy
+++ vars/readGlobalConfig.groovy
@@ -1,6 +1,7 @@
 @NonCPS
 def call(configName) {
 	def globalconfigfile
+	def conf
 	configFileProvider([configFile(fileId: 'global-pipeline-config', variable: 'conf')]) {
 		globalconfigfile = readFile(file: conf)
  	}
